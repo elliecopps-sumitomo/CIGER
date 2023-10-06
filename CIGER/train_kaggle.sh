54cb258ea01c9865a37dbcdc8bc67ee8e1729a51
Ellie Copps <ellie.copps@sumitovant.com>
2023-10-06 13:24:54 -0400

    ready to train with kaggle data

---
+++ CIGER/train_kaggle.sh
@@ -0,0 +1,5 @@
+CUDA_VISIBLE_DEVICES=0 python3 train.py --drug_file "../kaggle_data/drug_smiles_kaggle.csv" \
+--drug_id_file "../kaggle_data/drug_id_kaggle.csv" --gene_file "../kaggle_data/gene_feature_kaggle.csv" \
+--data_file "../kaggle_data/chemical_signature_kaggle.csv" --fp_type 'neural' --label_type 'real' --loss_type 'list_wise_rankcosine' \
+--batch_size 8 --max_epoch 40 --lr 0.003 --fold 0 --model_name 'kaggle_test' --warm_start 'False'  --inference 'False' \
+--kaggle 'True' > 'output/kaggle_40'
