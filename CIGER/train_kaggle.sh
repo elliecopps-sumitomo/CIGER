bea56e58b5322e95bd86b7995b57167cc5ef1aea
elliecopps-sumitomo <ellie.copps@sumitovant.com>
2023-10-13 20:23:37 +0000

    kaggle updates

--- CIGER/train_kaggle.sh
+++ CIGER/train_kaggle.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=0 python3 train.py --drug_file "../kaggle_data/drug_smiles_kaggle.csv" \
 --drug_id_file "../kaggle_data/drug_id_kaggle.csv" --gene_file "../kaggle_data/gene_feature_kaggle.csv" \
 --data_file "../kaggle_data/chemical_signature_kaggle.csv" --fp_type 'neural' --label_type 'real' --loss_type 'list_wise_rankcosine' \
---batch_size 8 --max_epoch 40 --lr 0.003 --fold 0 --model_name 'kaggle_test' --warm_start 'False'  --inference 'False' \
---kaggle 'True' > 'output/kaggle_40'
+--batch_size 64 --max_epoch 100 --lr 0.003 --fold 0 --model_name 'kaggle_100epoch_lr003' --warm_start 'False'  --inference 'False' \
+--kaggle 'True' > 'output/kaggle_100.txt'
